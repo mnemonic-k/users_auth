9d0ea727e24fc088b200ff921ebdec449b010c84
Yaroslav Kravchenko <yaroslav.kravchenko.main@gmail.com>
2024-11-02 01:05:10 +0200

    added docker

--- src/common/databases/databases.provider.ts
+++ src/common/databases/databases.provider.ts
@@ -1,12 +1,21 @@
 import { ConfigService } from '@nestjs/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { entities } from './databases.entities';
+import { Pool } from 'pg';
 
 export const databasesProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: async (config: ConfigService) => {
-      return drizzle(config.get('DATABASE_URL'), { schema: entities });
+    useFactory: async (configService: ConfigService) => {
+      const connection = new Pool({
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        user: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+      });
+
+      return drizzle(connection, { schema: entities });
     },
     inject: [ConfigService],
   },
--- src/sessions/sessions.module.ts
+++ src/sessions/sessions.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SessionController } from './sessions.controller';
 import { SessionsService } from './sessions.service';
 import { jwtModuleInstance } from 'src/common/jwt.module';
-import { redisModuleInstance } from 'src/common/redis.modulte';
+import { redisModuleInstance } from 'src/common/redis.module';
 import { DatabaseModule } from 'src/common/databases/database.module';
 
 @Module({
